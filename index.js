f2178230f3d191c4143354b2ea83080ee6106945
thana19 <thana.j@gmail.com>
2021-07-30 10:57:31 +0700

    add swagger

--- index.js
+++ index.js
@@ -24,11 +24,11 @@ app.get('/', (request, reply) => {
 //Mongoose ---------------------------------------
 
 mongoose.connect(config.mongodb.uri, {
-    useNewUrlParser: true, 
+    useNewUrlParser: true,
     useCreateIndex: true,
-    useUnifiedTopology: true 
+    useUnifiedTopology: true
 })
-     
+
 //CRUD no jwt---------------------------------------
 
 app.get('/users', async (request, reply) => {
@@ -60,27 +60,27 @@ app.post('/users', async (request, reply) => {
 
     const user = new User(doc)
     await user.save()
-    reply.send(user) 
+    reply.send(user)
 })
 
-app.post('/register', async (request, reply) => {
-    const doc = request.body
-    // console.log('request.body.password ->', request.body.password)
+// app.post('/register', async (request, reply) => {
+//     const doc = request.body
+//     // console.log('request.body.password ->', request.body.password)
 
-    doc.password = await generatePassword(request.body.password)
+//     doc.password = await generatePassword(request.body.password)
 
-    const user = new User(doc)
-    await user.save()
+//     const user = new User(doc)
+//     await user.save()
 
-    const profile = {
-        "userid": user._id,
-        "username": user.username,
-        "name": user.name,
-        "surname": user.surname,
-    }
+//     const profile = {
+//         "userid": user._id,
+//         "username": user.username,
+//         "name": user.name,
+//         "surname": user.surname,
+//     }
 
-    reply.send(profile) 
-})
+//     reply.send(profile) 
+// })
 
 const comparePassword = async (password, existsPassword) => {
     const isPasswordCorrect = await bcrypt.compare(password, existsPassword)
@@ -97,7 +97,7 @@ app.post('/login-x', async (request, reply) => {
 
     const user = await User.findOne({
         username
-    }) 
+    })
 
     if (!user) {
         throw new Error('unauthrized name')
@@ -112,17 +112,17 @@ app.patch('/users', async (request, reply) => {
         userId,
         name,
         surname
-    } = request.body    
+    } = request.body
 
     const updatedUser = await User.updateOne({
         _id: userId
-    } , {
+    }, {
         name,
         surname
-    },{
+    }, {
         returnOriginal: false
     })
- 
+
     reply.send(updatedUser)
 })
 
@@ -133,41 +133,41 @@ app.delete('/users', async (request, reply) => {
         _id: userId
     })
 
-    reply.send(result)    
+    reply.send(result)
 })
 
 //JWT ---------------------------------------
 
-app.post('/login', async (request, reply) => {
-    const { username, password } = request.body
+// app.post('/login', async (request, reply) => {
+//     const { username, password } = request.body
 
-    const user = await User.findOne({
-        username
-    }) 
+//     const user = await User.findOne({
+//         username
+//     }) 
 
-    if (!user) {
-        throw new Error('unauthrized name')
-    }
-    await comparePassword(password, user.password)
+//     if (!user) {
+//         throw new Error('unauthrized name')
+//     }
+//     await comparePassword(password, user.password)
 
-    const token = jwt.sign({
-        id: user._id
-    }, config.secretKey, {
-        expiresIn: 12000
-    })
+//     const token = jwt.sign({
+//         id: user._id
+//     }, config.secretKey, {
+//         expiresIn: 12000
+//     })
 
-    // return token
-    reply.send({'access_token':token})
-})
+//     // return token
+//     reply.send({'access_token':token})
+// })
 
-app.get('/getusers',{
+app.get('/getusers', {
     preHandler: [auth.validateToken]
 }, async (request, reply) => {
     const users = await User.find().lean()
     reply.send(users)
 })
 
-app.get('/getuser/:userId',{
+app.get('/getuser/:userId', {
     preHandler: [auth.validateToken]
 }, async (request, reply) => {
     const { userId } = request.params
@@ -177,7 +177,7 @@ app.get('/getuser/:userId',{
     reply.send(user)
 })
 
-app.post('/profile',{
+app.post('/profile', {
     preHandler: [auth.validateToken]
 }, async (request, reply) => {
     const { userId } = request.body
@@ -187,41 +187,41 @@ app.post('/profile',{
     reply.send(user)
 })
 
-app.get('/getprofile', async (request, reply) => {
-    const auth = request.headers.authorization;
-    const token = auth.split(' ')[1]
-    const { id } = jwt.verify(token, secretKey)
-    console.log('token2->', jwt.decode(token))
-    const user = await User.findById(id)
-    const profile = {
-        "userid": user._id,
-        "username": user.username,
-        "name": user.name,
-        "surname": user.surname,
-    }
-    reply.send(profile)
-})
+// app.get('/getprofile', async (request, reply) => {
+//     const auth = request.headers.authorization;
+//     const token = auth.split(' ')[1]
+//     const { id } = jwt.verify(token, secretKey)
+//     console.log('token2->', jwt.decode(token))
+//     const user = await User.findById(id)
+//     const profile = {
+//         "userid": user._id,
+//         "username": user.username,
+//         "name": user.name,
+//         "surname": user.surname,
+//     }
+//     reply.send(profile)
+// })
 
-app.patch('/updateprofile',{
-    preHandler: [auth.validateToken]
-}, async (request, reply) => {
-    const {
-        userId,
-        name,
-        surname
-    } = request.body    
+// app.patch('/updateprofile', {
+//     preHandler: [auth.validateToken]
+// }, async (request, reply) => {
+//     const {
+//         userId,
+//         name,
+//         surname
+//     } = request.body
 
-    const updatedUser = await User.updateOne({
-        _id: userId
-    } , {
-        name,
-        surname
-    },{
-        returnOriginal: false
-    })
- 
-    reply.send(updatedUser)
-})
+//     const updatedUser = await User.updateOne({
+//         _id: userId
+//     }, {
+//         name,
+//         surname
+//     }, {
+//         returnOriginal: false
+//     })
+
+//     reply.send(updatedUser)
+// })
 
 //Swagger ---------------------------------------
 
@@ -229,10 +229,271 @@ app.register(FastifySwagger, {
     routePrefix: '/documents',
     swagger: {
         info: {
-            title: 'Node-Mongo-JWT LLDD',
-            description: 'CRUD+JWT',
+            title: 'api.thana.in.th LLDD',
+            description: 'Workshop',
             version: '1.0'
-        }
+        },
+        securityDefinitions: {
+            bearerAuth: {
+              type: 'apiKey',
+              scheme: 'bearer',
+              bearerFormat: 'JWT',
+              description: 'JWT authorization of an API',
+              name: 'Authorization',
+              in: 'header',
+            },
+          },
+        
     },
     exposeRoute: true
 })
+
+//Register
+
+const registerRoute = {
+    method: 'POST',
+    url: '/register',
+    schema: {
+        body: {
+            username: {
+                type: 'string'
+            },
+            password: {
+                type: 'string'
+            },
+            name: {
+                type: 'string'
+            },
+            surname: {
+                type: 'string'
+            }
+        },
+        response: {
+            200: {
+                type: 'object',
+                properties: {
+                    // userid: {
+                    //     type: 'string'
+                    // },
+                    username: {
+                        type: 'string'
+                    },
+                    name: {
+                        type: 'string'
+                    },
+                    surname: {
+                        type: 'string'
+                    }
+                }
+            }
+        }
+    },
+    handler: async (request, reply) => {
+        const doc = request.body
+        // console.log('request.body.password ->', request.body.password)
+        doc.password = await generatePassword(request.body.password)
+
+        const user = new User(doc)
+        await user.save()
+
+        const profile = {
+            // "userid": user._id,
+            "username": user.username,
+            "name": user.name,
+            "surname": user.surname,
+        }
+
+        reply.send(profile)
+    }
+}
+app.route(registerRoute)
+
+//login
+
+const logineRoute = {
+    method: 'POST',
+    url: '/login',
+    schema: {
+        body: {
+            username: {
+                type: 'string'
+            },
+            password: {
+                type: 'string'
+            },
+            // name: {
+            //     type: 'string'
+            // },
+            // surname: {
+            //     type: 'string'
+            // }
+        },
+        response: {
+            200: {
+                type: 'object',
+                properties: {
+                    access_token: {
+                        type: 'string'
+                    },
+                }
+            }
+        }
+    },
+    handler: async (request, reply) => {
+        const { username, password } = request.body
+
+        const user = await User.findOne({
+            username
+        })
+
+        if (!user) {
+            throw new Error('unauthrized name')
+        }
+        await comparePassword(password, user.password)
+
+        const token = jwt.sign({
+            id: user._id
+        }, config.secretKey, {
+            expiresIn: 12000
+        })
+
+        // return token
+        reply.send({ 'access_token': token })
+    }
+}
+app.route(logineRoute)
+
+//getprofile
+const getProfileRoute = {
+    method: 'GET',
+    url: '/getprofile',
+    schema: {
+        // body: {
+        //     username: {
+        //         type: 'string'
+        //     },
+        //     password: {
+        //         type: 'string'
+        //     },
+        //     name: {
+        //         type: 'string'
+        //     },
+        //     surname: {
+        //         type: 'string'
+        //     }
+        // },
+        response: {
+            200: {
+                type: 'object',
+                properties: {
+                    // userid: {
+                    //     type: 'string'
+                    // },
+                    username: {
+                        type: 'string'
+                    },
+                    name: {
+                        type: 'string'
+                    },
+                    surname: {
+                        type: 'string'
+                    }
+                }
+            }
+        },
+        security: [
+            {
+            'bearerAuth': []
+            }
+        ]
+    },
+    handler: async (request, reply) => {
+        const auth = request.headers.authorization;
+        const token = auth.split(' ')[1]
+        const { id } = jwt.verify(token, secretKey)
+        console.log('token2->', jwt.decode(token))
+        const user = await User.findById(id)
+        const profile = {
+            // "userid": user._id,
+            "username": user.username,
+            "name": user.name,
+            "surname": user.surname,
+        }
+        reply.send(profile)
+    }
+}
+app.route(getProfileRoute)
+
+//updateprofile
+const updateProfileRoute = {
+    method: 'PATCH',
+    url: '/updateprofile',
+    schema: {
+        body: {
+            // userid: {
+            //     type: 'string'
+            // },
+            // password: {
+            //     type: 'string'
+            // },
+            name: {
+                type: 'string'
+            },
+            surname: {
+                type: 'string'
+            }
+        },
+        response: {
+            200: {
+                type: 'object',
+                properties: {
+                    userid: {
+                        type: 'string'
+                    },
+                    // username: {
+                    //     type: 'string'
+                    // },
+                    name: {
+                        type: 'string'
+                    },
+                    surname: {
+                        type: 'string'
+                    }
+                }
+            }
+        },
+        security: [
+            {
+            'bearerAuth': []
+        }
+    ]
+    },
+  
+    // preHandler: [auth.validateToken],
+    handler: async (request, reply) => {
+        const auth = request.headers.authorization;
+        const token = auth.split(' ')[1]
+        const { id } = jwt.verify(token, secretKey)
+        const user = await User.findById(id)
+        console.log('token2->', auth)
+        console.log('token2->', jwt.decode(token))
+
+        const {
+            userId,
+            name,
+            surname
+        } = request.body
+        console.log(request.body)
+        const updatedUser = await User.updateOne({
+            _id: user._id
+        }, {
+            name,
+            surname
+        }, {
+            returnOriginal: false
+        })
+
+        reply.send(updatedUser)
+    }
+}
+app.route(updateProfileRoute)
